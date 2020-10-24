5cd3f1a9eb111ed1b63cfe5c62cc01012674ea53
primemini <vjvitale@buffalo.edu>
2020-10-24 15:23:19 -0400

    created pet, pet states, and functionality for interacting with pet

---
+++ PetClass.py
@@ -0,0 +1,40 @@
+from PetTypes import PetTypes
+from PetStates import PetStates
+
+class Pet:
+
+	def __init__(self, petType):
+
+		self.m_fullness = 50
+		self.m_happiness = 75
+		self.m_petType = petType
+		self.m_petState = PetStates.CONTENT
+
+	def feedPet(self, amount):
+		self.m_fullness += amount
+		if self.m_fullness >= 100:
+			self.m_happiness -= self.m_fullness - 100
+		else:
+			self.m_happiness += amount//5
+
+	def playWithPet(self, amount):
+		self.m_happiness = (self.m_happiness + amount)%100
+		self.m_fullness -= amount//5
+		if self.m_fullness < 0:
+			self.m_fullness = 0
+
+	def updatePetState(self):
+		mood = self.m_happiness
+		if mood >= 75:
+			self.m_petState = PetStates.HAPPY
+		elif mood >= 50:
+			self.m_petState = PetStates.CONTENT
+		elif mood == 0 and self.m_fullness == 0:
+			self.m_petState = PetStates.DEAD
+		else:
+			self.m_petState = PetStates.SAD
+
+
+
+
+
---
+++ PetTypes.py
@@ -0,0 +1,7 @@
+from enum import Enum
+
+#these are the different pet types
+class PetTypes(Enum):
+	ANIMAL_A = 1
+	ANIMAL_B = 2
+	ANIMAL_C = 3
