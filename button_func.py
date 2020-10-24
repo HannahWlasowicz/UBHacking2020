0758be2ba419ff0c67a93e5e2bc333d0760ed4ea
Hannah Wlasowicz <hannahwl@buffalo.edu>
2020-10-24 15:01:32 -0400

    added all photos

---
+++ button_func.py
@@ -0,0 +1,11 @@
+def feedAction():
+    print("YO")
+
+def walkAction():
+    print("wee")
+
+def petAction():
+    print("good boi")
+
+def connectAction():
+    print("for later")
--- gui.py
+++ gui.py
@@ -1,12 +1,11 @@
 import tkinter as tk
+from button_func import *
 from tkinter import filedialog, Text, RIGHT, BOTH, LEFT
 import os
 
 root = tk.Tk()
 root.title("Tamagotchi")
 
-def addApp():
-    print("HI :)")
     
 canvas = tk.Canvas(root, height = 700, width=700, bg = "#038cfc")
 canvas.grid(row = 0, columnspan =4)
@@ -19,21 +18,13 @@ frame.grid(row=1,columnspan =4)
 # frame.grid(row = 0, column = 0)
 # frame.grid()
 
-b1 = tk.Button(root,text = "Feed", padx = 10, pady =5, fg ="white", bg="#038cfc", command = addApp)
-# left.pack(side = RIGHT)
+b1 = tk.Button(root,text = "Feed", padx = 10, pady =5, fg ="white", bg="#038cfc", command = feedAction)
 
+b2 = tk.Button(root,text = "Walk", padx = 10, pady =5, fg ="white", bg="#038cfc", command = walkAction)
 
+b3 = tk.Button(root,text = "Pet", padx = 10, pady =5, fg ="white", bg="#038cfc", command = petAction)
 
-b2 = tk.Button(root,text = "Walk", padx = 10, pady =5, fg ="white", bg="#038cfc")
-# middle.pack()
-
-
-# second_middle.pack()
-
-b3 = tk.Button(root,text = "Pet", padx = 10, pady =5, fg ="white", bg="#038cfc")
-# right.pack(side = LEFT)
-
-b4 = tk.Button(root,text = "Connect", padx = 10, pady =5, fg ="white", bg="#038cfc")
+b4 = tk.Button(root,text = "Connect", padx = 10, pady =5, fg ="white", bg="#038cfc", command = connectAction)
 
 
 
