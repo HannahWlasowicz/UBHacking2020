cd3717cabf088233fe40d5d9354976b8a75ac932
Hannah Wlasowicz <hannahwl@buffalo.edu>
2020-10-24 14:53:27 -0400

    UI items + photos

--- gui.py
+++ gui.py
@@ -1,27 +1,49 @@
 import tkinter as tk
-from tkinter import filedialog, Text
+from tkinter import filedialog, Text, RIGHT, BOTH, LEFT
 import os
 
 root = tk.Tk()
+root.title("Tamagotchi")
 
 def addApp():
     print("HI :)")
     
 canvas = tk.Canvas(root, height = 700, width=700, bg = "#038cfc")
-canvas.pack()
+canvas.grid(row = 0, columnspan =4)
 
 frame = tk.Frame(root, bg="white")
-frame.place(relwidth = 0.8, relheight=0.8, relx = 0.1, rely = 0.1)
+frame.grid(row=1,columnspan =4)
 
 
-left = tk.Button(root,text = "Right", padx = 10, pady =5, fg ="white", bg="#038cfc", command = addApp)
-left.pack()
+# frame.place(relwidth = 0.8, relheight=0.8, relx = 0.1, rely = 0.1)
+# frame.grid(row = 0, column = 0)
+# frame.grid()
 
-middle = tk.Button(root,text = "Middle", padx = 10, pady =5, fg ="white", bg="#038cfc")
-middle.pack()
+b1 = tk.Button(root,text = "Feed", padx = 10, pady =5, fg ="white", bg="#038cfc", command = addApp)
+# left.pack(side = RIGHT)
 
-right = tk.Button(root,text = "Left", padx = 10, pady =5, fg ="white", bg="#038cfc")
-right.pack()
+
+
+b2 = tk.Button(root,text = "Walk", padx = 10, pady =5, fg ="white", bg="#038cfc")
+# middle.pack()
+
+
+# second_middle.pack()
+
+b3 = tk.Button(root,text = "Pet", padx = 10, pady =5, fg ="white", bg="#038cfc")
+# right.pack(side = LEFT)
+
+b4 = tk.Button(root,text = "Connect", padx = 10, pady =5, fg ="white", bg="#038cfc")
+
+
+
+b1.grid(row = 2, column = 0)
+b2.grid(row = 2, column = 1)
+b3.grid(row = 2, column = 2)
+b4.grid(row = 2, column = 3)
+
+
+
 
 
 
